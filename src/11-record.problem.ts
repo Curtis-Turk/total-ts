a15dca2874ee362e1dcb466bb1926432c4d292f8
Curtis Turk <curtis.c.turk@outlook.com>
2023-02-23 17:55:20 +0000

    Completed exercise 11

--- src/11-record.problem.ts
+++ src/11-record.problem.ts
@@ -1,7 +1,11 @@
 import { expect, it } from "vitest";
 
+type cacheId = {
+  [id: string]: string;
+};
+
 const createCache = () => {
-  const cache = {};
+  const cache: cacheId = {};
 
   const add = (id: string, value: string) => {
     cache[id] = value;
